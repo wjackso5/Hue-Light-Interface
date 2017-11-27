2ca42042a9a7117dbe6de9e9190011ff10e5b5b4
William Craig Jackson <wjackso5@cs3307-pub.gaul.csd.uwo.ca>
2017-11-27 17:17:32 -0500

    lm???

---
+++ src/Light_Manager.cpp
@@ -0,0 +1,151 @@
+/* Light Manager Class
+ *
+ * Controller Class for Light, editing Lights, etc.
+ *
+ * FINISHED
+ */
+
+
+/* FROM STL */
+#include <string>
+#include <vector>
+#include <Wt/Http/Client>
+#include <Wt/Http/Message>
+
+/* LOCAL FILES */
+#include "Light_Manager"
+
+
+class Light_Manager {
+		Light_Manager::Light_Manager(Bridge* b)
+		{
+			bridge=b;
+		}
+		Light_Manager::~Light_Manager()
+		{
+			free(bridge);
+		}
+
+		//PUBLIC METHODS
+		bool Light_Manager::getLights(){
+			Wt::Http::Client *httpC = new Wt::Http::Client;
+			std::string url;
+			httpC->done().connect(boost::bind(&Light_Manager::handleLightResponse,this,_1,_2));
+			url = "http://" + bridge->ip + ':' + bridge->port + "/api/"+bridge->usernmae+"/lights";
+			if(httpC->get(url)){
+				WApplication::instance()->deferRendering();
+				return true;
+			}
+			free(httpC);
+			return false;
+		}
+
+		bool Light_Manager::getGroups(){
+			Wt::Http::Client *httpC = new Wt::Http::Client;
+			std::string url;
+			httpC->done().connect(boost::bind(&Light_Manager::handleHttpResponse,this,_1,_2));
+			url = "http://" + bridge->ip + ':' + bridge->port + "/api/"+bridge->usernmae+"/groups";
+			if(httpC->get(url)){
+				WApplication::instance()->deferRendering();
+				return true;
+			}
+			free(httpC);
+			return false;
+		}
+		
+		bool Light_Manager::setLightName(string id,std::string name){
+			Wt::Http::Client *httpC = new Wt::Http::Client;
+			std::string body="{\"name\" : \""+name+"\"}";
+			std::string url;
+			Wt::Http::Message *message=new Wt::Http:Message;
+			message->addBodyText(body);
+			httpC->done().connect(boost::bind(&Light_Manager::handleHttpResponse,this,_1,_2));
+			url="http://" + bridge->ip + ':' + bridge->port + "/api/"+bridge->usernmae+"/lights/"+id;
+			if(httpC->put(url,message)){
+				WApplication::instance()->deferRendering();
+				return true;
+			}
+			free(httpC);
+			return false;
+		}
+
+		bool Light_Manager::setLightState(std::string id,std::string statename,std::string state){
+			Wt::Http::Client *httpC = new Wt::Http::Client;
+			std::string body="{\""+statename+\" : "+ state+"}";
+			std::string url;
+			Wt::Http::Message *message=new Wt::Http:Message;
+			message.setHearder("Content-type","application/Json");
+			message->addBodyText(body);
+			httpC->done().connect(boost::bind(&Light_Manager::handleHttpResponse,this,_1,_2));
+			url="http://" + bridge->ip + ':' + bridge->port + "/api/"+bridge->usernmae+"/lights/"+id+"/state";
+			if(httpC->put(url,*message)){
+				WApplication::instance()->deferRendering();
+				return true;
+			}
+			return false;
+		}
+
+		// bool createGroup(std::string ids,std::string name,){
+
+		// }
+
+		// bool setGroup()
+
+		void Light_Manager::handleLightResponse(boost::system::error_code err,const Wt::Http::Message& response)
+		{
+			Wt::log("HANDLE")<<"HANDLING LIGHT LISTS";
+			if (!err && response.status() == 200) {
+				const std::string &input = response.body();
+				Wt::log("LIGHTINFO") << input;
+				//Wt::Json::Value *jval = new Wt::Json::Value();
+				//Wt::Json::Value &jvalref = jval; 
+				Wt::Json::Object ob;
+				int size=ob.size();
+				for (int i=0;i<size;i++){
+
+					Wt::Json::Object val=ob.get(std::to_string(i+1));
+					std::string light_name=val.get("name").toString().orIfNull(WString("name:((("));
+					Wt::Json::Value states=val.get("state")
+					boolean isOn=states.get("on").toBool().orIfNull(false);
+					int light_brightness=states.get("bri").toNumber().orIfNull(-11111);
+					int light_hue=states.get("hue").toNumber().orIfNull(-222222);
+					Light *l=new Light(i+1,light_name,isOn,light_brightness,light_hue);
+					lightlist.push(l);
+					/*on,bri,*/
+				}
+			}
+   		}
+
+   		void Light_Manager::handleGroupResponse(boost::system::error_code err,const Wt::Http::Message& response)
+		{
+			Wt::log("HANDLE")<<"HANDLING GROUOP LISTS";
+			if (!err && response.status() == 200) {
+				const std::string &input = response.body();
+				Wt::log("LIGHTINFO") << input;
+				Wt::Json::Object ob;
+				int size=ob.size();
+				for (int i=0;i<size;i++){
+					Wt::Json::Value val=ob.get(std::to_string(i+1));
+					std::string light_name=val.get("name");
+					Wt::Json::Value states=val.get("state");
+					boolean isOn=states.get("on");
+					int light_brightness=states.get("bri");
+					int light_hue=states.get("hue");
+					Light *l=new Light(i+1,light_name,isOn,light_brightness,light_hue);
+					lightlist.push(l);
+					/*on,bri,*/
+				}
+
+				
+			}
+   		}
+
+   		Bridge* Light_Manager::getBridge()
+   		{
+   			return bridge;
+   		}
+
+}
+
+
+		
