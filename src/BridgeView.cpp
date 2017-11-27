a1c2744dc9625b6a7be95871a9b695a5cfbafab4
William Craig Jackson <wjackso5@uwo.ca>
2017-11-27 15:04:53 -0500

    debugging merge #11

--- src/BridgeView.cpp
+++ src/BridgeView.cpp
@@ -182,9 +182,10 @@ void BridgeView::showBridgeList(){
 
   void BridgeView::createLightView(){
     int index = bm->findBridge(btv_name->text().toUTF8());
-    *btv = *bl->at(index);
+    Bridge btv = new Bridge();
+    btv = *bl->at(index);
     Wt::log("info") << btv->getName();
     //addWidget(new LightView(btv));
-    this->hide();
-    this->clear();
+    //this->hide();
+    //this->clear();
   }
