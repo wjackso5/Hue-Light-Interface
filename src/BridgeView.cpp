1a5b8475fb17321fd96ae117796b09a135c8ad88
William Craig Jackson <wjackso5@uwo.ca>
2017-11-26 22:42:16 -0500

    testing wt table

--- src/BridgeView.cpp
+++ src/BridgeView.cpp
@@ -152,9 +152,9 @@ void BridgeView::showBridgeList(){
       bridge_list_->elementAt(i+1, 3)->addWidget(new WText(bl.at(i)->getPort()));
       bridge_list_->elementAt(i+1, 4)->addWidget(new WText(bl.at(i)->getUsername()));
       index = i;
-      WPushButton *goto_bridge_button = new WPushButton("View");
+      /*WPushButton *goto_bridge_button = new WPushButton("View");
       bridge_list_->elementAt(i+1, 4)->addWidget(goto_bridge_button);
-      goto_bridge_button->clicked().connect(this, &BridgeView::createLightView);
+      goto_bridge_button->clicked().connect(this, &BridgeView::createLightView);*/
   }
   addWidget(bridge_list_);
 }
