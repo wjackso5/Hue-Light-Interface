52240ccf67cafcd740dbc79b48ce9f8b699814d0
William Craig Jackson <wjackso5@uwo.ca>
2017-11-27 17:53:00 -0500

    added views back

---
+++ include/GroupView.h
@@ -0,0 +1,27 @@
+//GroupView Headerfile
+#ifndef GroupView_H_
+#define GroupView_H_
+#include <Wt/WContainerWidget>
+#include <Wt/WTable>
+#include "Light.h"
+//#include "Light_Manager.h"
+#include "Bridge.h"
+class GroupView : public Wt::WContainerWidget
+{
+public:
+  GroupView(Light_Manager *lm);
+private:
+  Light_Manager *lm;
+  //Light_Manager *lm;
+  std::vector<LightGroup*> gl;
+  Wt::WTable *group_list_;
+  Wt::WText *group_msg_;
+  Wt::WLineEdit *group_id_;
+  Wt::WLineEdit *group_state_;
+  Wt::WPushButton *group_button_;
+  Wt::WPushButton *goto_bridgeview_button;
+  void clearFields();
+  void showGroupList();
+  void UpdateGroup();
+};
+#endif
