f7a13e13cc8f76dd48682d0fccc72ccda7cf9a4e
Maya Emilia Bielecki <mbielec3@cs3307-pub.gaul.csd.uwo.ca>
2017-11-27 19:13:37 -0500

    added an empty but working SpotifyView

--- src/SpotifyView.cpp
+++ src/SpotifyView.cpp
@@ -1,27 +1,37 @@
+#include <Wt/WApplication>
+#include <Wt/WContainerWidget>
+#include <Wt/Json/Parser>
+#include <Wt/Json/Value>
+#include <Wt/Json/Object>
+#include <Wt/WText>
+#include "SpotifyView.h"
+
 using namespace Wt;
 
 SpotifyView::SpotifyView(WContainerWidget *parent):WContainerWidget(parent)
 {
 
-}
+WText *title = new WText("<h1>Spotify Music Sync:</h1>");
+addWidget(title);
+addWidget(new WBreak());
 
-char client_id[] = '9bc9cc02a0824d7eaed59d5e652c3bab'; // Our Spotify Client id
-char client_secret[] = '131449d1316e4ceaa38b655787d9591c'; // Our Spotify Client secret
-char redirect_uri[] = 'REDIRECT_URI'; // Our redirect uri, should bring us back to main view, to be done in future
+char client_id[37] = {"9bc9cc02a0824d7eaed59d5e652c3bab"}; // Our Spotify Client id
+char client_secret[37] = {"131449d1316e4ceaa38b655787d9591c"}; // Our Spotify Client secret
+char redirect_uri[15] = {"REDIRECT_URI"}; // Our redirect uri, should bring us back to main view, to be done in future
 
-//char state[] = Generate a random string containing numbers and letters
+char state[37] = {""}; //Generate a random string containing numbers and letters
 
-char stateKey[] = 'spotify_auth_state';
-/*
-WApplication::setCookie(statekey, state, 1800)
+char stateKey[20] = {"spotify_auth_state"};
+
+//WApplication.setCookie(stateKey, state, 1800);
 
 //const std::string & 	domain = "",
 //const std::string & 	path = "",
 //bool 	secure = false 
 //name is statekey, value is state
 
-char scope[] = 'user-read-private user-read-email'; //the application requests authorization
-
+char scope[37] = {"user-read-private user-read-email"}; //the application requests authorization
+}
 /*
 
   res.redirect('https://accounts.spotify.com/authorize?' +
--- src/SpotifyView.h
+++ src/SpotifyView.h
@@ -1,15 +1,13 @@
 #ifndef SpotifyView_H_
 #define SpotifyView_H_
 #include "DBSession.h"
-
-namespace Wt {
-  class WStackedWidget;
-  class WAnchor;
-  class WContainerWidget;
-}
+#include <Wt/WContainerWidget>
 
 
 class SpotifyView : public Wt::WContainerWidget
 {
 public:
   SpotifyView(Wt::WContainerWidget *parent = 0);
+};
+
+#endif//SpotifyView_H_
