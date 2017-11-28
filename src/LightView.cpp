22ce90df7c499fa2de72938d86bd2d0b758e2a68
William Craig Jackson <wjackso5@uwo.ca>
2017-11-27 19:50:23 -0500

    reintegrating LM after fixing bug #6

--- src/LightView.cpp
+++ src/LightView.cpp
@@ -89,12 +89,12 @@ void LightView::showLightList(){
   //get the lightlist
   ll = lm->getLightList();
   //populate the table with the info from the lightlist.
-  for(int i=0; i<ll.size(); i++){
-      light_list_->elementAt(i+1, 0)->addWidget(new WText(llat(i)->getId()));
+  for(int i=0; i<ll->size(); i++){
+      light_list_->elementAt(i+1, 0)->addWidget(new WText(std::to_string(ll->at(i)->getId())));
       light_list_->elementAt(i+1, 1)->addWidget(new WText(ll->at(i)->getName()));
-      light_list_->elementAt(i+1, 2)->addWidget(new WText(ll->at(i)->getSwitch()));
-      light_list_->elementAt(i+1, 3)->addWidget(new WText(ll->at(i)->getColor()));
-      light_list_->elementAt(i+1, 4)->addWidget(new WText(ll->at(i)->getBrightness()));
+      light_list_->elementAt(i+1, 2)->addWidget(new WText(std::to_string(ll->at(i)->getSwitch())));
+      light_list_->elementAt(i+1, 3)->addWidget(new WText(std::to_string(ll->at(i)->getColor())));
+      light_list_->elementAt(i+1, 4)->addWidget(new WText(std::to_string(ll->at(i)->getBrightness())));
   }
   addWidget(light_list_);
 }
