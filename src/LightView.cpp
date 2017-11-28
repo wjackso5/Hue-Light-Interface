b42c50e8cc3e9739cc27b8e2e9c3389b88f44da5
William Craig Jackson <wjackso5@uwo.ca>
2017-11-27 22:39:38 -0500

    Fixed Segfault for light table, trying to make it actually populate

--- src/LightView.cpp
+++ src/LightView.cpp
@@ -91,7 +91,9 @@ void LightView::showLightList(){
   light_list_->elementAt(0, 3)->addWidget(new WText("Color"));
   light_list_->elementAt(0, 4)->addWidget(new WText("Brightness"));
   //get the lightlist
-  /* ll = lm->getlightList();
+  Wt::log("DEBUG")<<"!!!!!!60!!!!!! aft add group and sched views";
+  ll = lm->getlightList();
+  /*
   //populate the table with the info from the lightlist.
   for(int i=0; i<ll.size(); i++){
       light_list_->elementAt(i+1, 0)->addWidget(new WText(ll->at(i)->getName()));
