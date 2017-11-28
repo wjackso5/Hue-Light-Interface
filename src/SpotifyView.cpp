03711bc2e4d4b2efb3e5eefbee25d898688f1fce
mbielec3 <mbielec3@uwo.ca>
2017-11-28 16:30:00 -0600

    vvvv

--- src/SpotifyView.cpp
+++ src/SpotifyView.cpp
@@ -11,6 +11,7 @@
 #include <Wt/WPushButton>
 #include <Wt/WAnchor>
 #include <Wt/WStackedWidget>
+#include <Wt/WLink>
 #include "SpotifyView.h"
 #include <string>
 
@@ -32,12 +33,7 @@ SpotifyView::SpotifyView():WContainerWidget(){
   char scope[37] = {"user-read-private user-read-email"}; //the application requests authorization
 */
   
-  //Set up HTTP client
-  Http::Client *client = new Http::Client(this);
-  client->setTimeout(15);
-  client->setMaximumResponseSize(10 * 1024);
-  client->done().connect(boost::bind(&SpotifyView::handleHttpResponse, this, _1, _2));
-  client->setFollowRedirect(true); 
+  
 
   //Adding Button
   Wt::log("SPOTIFY")<<"Ready to Create Button";
@@ -47,15 +43,8 @@ SpotifyView::SpotifyView():WContainerWidget(){
   Wt::log("SPOTIFY")<<"Button Added, Ready to Link Function";
   spotify_login_button->clicked().connect(this, &SpotifyView::spotifyLogIn);
   Wt::log("SPOTIFY")<<"Function Added";
-/*
-  //Make get call to the Spotify Authorization
-  if (client->get("https://accounts.spotify.com/en/authorize?%20&response_type=code&client_id=9bc9cc02a0824d7eaed59d5e652c3bab&scope=user-read-private%20user-read-email&redirect_uri=localhost:10026&state=state")){
-       //WApplication::instance()->deferRendering();
-       std::cerr << "No error." << std::endl;
-  }else {
-       std::cerr << "URL Error!" << std::endl;
-  }
-*/
+
+
 }
   void SpotifyView::handleHttpResponse(boost::system::error_code err, const Http::Message& response)
   {
@@ -66,8 +55,31 @@ SpotifyView::SpotifyView():WContainerWidget(){
   }
 
   void SpotifyView::spotifyLogIn(){
-  WApplication::instance()->redirect("https://accounts.spotify.com/en/authorize?%20&response_type=code&client_id=9bc9cc02a0824d7eaed59d5e652c3bab&scope=user-read-private%20user-read-email&redirect_uri=http://0.0.0.0:10026/&state=state");
+    //Set up HTTP client
+  Http::Client *client = new Http::Client(this);
+  client->setTimeout(15);
+  client->setMaximumResponseSize(10 * 1024);
+  client->done().connect(boost::bind(&SpotifyView::handleHttpResponse, this, _1, _2));
+  client->setFollowRedirect(true); 
+  //Make get call to the Spotify Authorization
+  
+  WAnchor *login;
+//Not sure which method to use! I'm supposed to get an auth code from it, so get make sense, but get doesn't redirect the user.
+  //the WAnchor method causes an error on login :(
+  login = new WAnchor("https://accounts.spotify.com/en/authorize?%20&response_type=code&client_id=9bc9cc02a0824d7eaed59d5e652c3bab&scope=user-read-private%20user-read-email&redirect_uri=localhost:10026&state=state", "Spotify Log In", this);
+  login -> setTarget(TargetNewWindow);
+
+  if (client->get("https://accounts.spotify.com/en/authorize?%20&response_type=code&client_id=9bc9cc02a0824d7eaed59d5e652c3bab&scope=user-read-private%20user-read-email&redirect_uri=localhost:10026&state=state")){
+       //WApplication::instance()->deferRendering();
+       std::cerr << "No error." << std::endl;
+  }else {
+       std::cerr << "URL Error!" << std::endl;
+  }
+
+
+  /*WApplication::instance()->redirect("https://accounts.spotify.com/en/authorize?%20&response_type=code&client_id=9bc9cc02a0824d7eaed59d5e652c3bab&scope=user-read-private%20user-read-email&redirect_uri=http://0.0.0.0:10026/&state=state");
   WApplication::instance()->refresh();
+  */
   }
 
 
