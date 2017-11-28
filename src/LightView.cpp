ef9d94326e440957915fee839dff6b4d028ee176
William Craig Jackson <wjackso5@uwo.ca>
2017-11-27 21:28:25 -0500

    debugging lightview showing lightlist#2

--- src/LightView.cpp
+++ src/LightView.cpp
@@ -91,13 +91,13 @@ void LightView::showLightList(){
   ll = lm->getLightList();
   //populate the table with the info from the lightlist.
   Wt::log("LLSIZE") << ll->size();
-  for(int i=0; i<ll->size(); i++){
+  //for(int i=0; i<ll->size(); i++){
       /*light_list_->elementAt(i+1, 0)->addWidget(new WText(std::to_string(ll->at(i)->getId())));
       light_list_->elementAt(i+1, 1)->addWidget(new WText(ll->at(i)->getName()));
       light_list_->elementAt(i+1, 2)->addWidget(new WText(std::to_string(ll->at(i)->getSwitch())));
       light_list_->elementAt(i+1, 3)->addWidget(new WText(std::to_string(ll->at(i)->getColor())));
       light_list_->elementAt(i+1, 4)->addWidget(new WText(std::to_string(ll->at(i)->getBrightness())));*/
-  }
+  //}
   addWidget(light_list_);
 }
   void LightView::clearView(){
