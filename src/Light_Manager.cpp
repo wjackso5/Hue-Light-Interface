5fb0e67be65592a47654d84f13b334f540c517a1
William Craig Jackson <wjackso5@uwo.ca>
2017-11-27 22:55:25 -0500

    lights json is parsed correctly but not added to lightList

--- src/Light_Manager.cpp
+++ src/Light_Manager.cpp
@@ -127,9 +127,8 @@
 					int light_brightness=states.get("bri").toNumber().orIfNull(-11111);
 					int light_hue=states.get("hue").toNumber().orIfNull(-222222);
 					Light *l = new Light(i+1,light_name,isOn,light_brightness,light_hue,bridge->username);
-					Wt::log("LIGHTINFO") << l->getName();
-					lightList->push_back(new Light(i+1,light_name,isOn,light_brightness,light_hue,bridge->username));
-					Wt::log("LIGHTINFO") << "PUSHED TO BACK OF LIGHTLIST";
+					Wt::log("LIGHTINFO") << l->getName()+ "Pushed to back";
+					lightList->push_back(l);
 					/*on,bri,*/
 				}
 			}
