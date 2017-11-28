ac4bf4edfb3c5f530c97bc32fb208e9acfc35b7d
mbielec3 <mbielec3@uwo.ca>
2017-11-28 06:20:28 -0600

    vvvvvv'

--- src/SpotifyView.cpp
+++ src/SpotifyView.cpp
@@ -32,8 +32,11 @@ SpotifyView::SpotifyView():WContainerWidget(){
   client->setFollowRedirect(true); 
 
   //User logs in
-  WApplication::instance()->redirect("https://accounts.spotify.com/en/authorize?%20&response_type=code&client_id=9bc9cc02a0824d7eaed59d5e652c3bab&scope=user-read-private%20user-read-email&redirect_uri=localhost:10026&state=state");
+  WApplication::instance()->redirect("https://accounts.spotify.com/en/authorize?%20&response_type=code&client_id=9bc9cc02a0824d7eaed59d5e652c3bab&scope=user-read-private%20user-read-email&redirect_uri=http://0.0.0.0:10026/&state=state");
 
+  client->get("/callback");
+
+/*
   //Make get call to the Spotify Authorization
   if (client->get("https://accounts.spotify.com/en/authorize?%20&response_type=code&client_id=9bc9cc02a0824d7eaed59d5e652c3bab&scope=user-read-private%20user-read-email&redirect_uri=localhost:10026&state=state")){
        //WApplication::instance()->deferRendering();
@@ -41,6 +44,7 @@ SpotifyView::SpotifyView():WContainerWidget(){
   }else {
        std::cerr << "URL Error!" << std::endl;
   }
+*/
 }
   void SpotifyView::handleHttpResponse(boost::system::error_code err, const Http::Message& response)
   {
